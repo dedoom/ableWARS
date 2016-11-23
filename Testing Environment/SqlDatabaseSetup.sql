d73ea3420feae90908efbb13153bd682dfe8bb34
avery morbi <averymorbi@gmail.com>
2016-11-23 15:47:26 -0500

    update db script

--- Testing Environment/SqlDatabaseSetup.sql
+++ Testing Environment/SqlDatabaseSetup.sql
@@ -8,8 +8,8 @@ GO
 CREATE TABLE dbo.account 
 (  
     username varchar(30) PRIMARY KEY NOT NULL  
-    ,fName varchar(30) NOT NULL  
-    ,lName varchar(30) NOT NULL  
+    ,fName varchar(30) NULL  
+    ,lName varchar(30) NULL  
     ,password varchar(30) NOT NULL  
     ,teamid varchar(10) NULL
 ); 

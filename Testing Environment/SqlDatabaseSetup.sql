a99cb94dbdcadb9388d6c43bd59e4cb879317107
Bil Al-Shaikh <bashaikh11@gmail.com>
2016-11-23 15:33:59 -0500

    Database Setup Script
    Run the script under localhost

---
+++ Testing Environment/SqlDatabaseSetup.sql
@@ -0,0 +1,15 @@
+USE master;  
+GO  
+CREATE DATABASE AbleWars;
+
+use [AbleWars]
+GO
+
+CREATE TABLE dbo.PurchaseOrderDetail  
+(  
+    username varchar(30) NOT NULL  
+    ,fName varchar(30) NOT NULL  
+    ,lName varchar(30) NOT NULL  
+    ,password varchar(30) NOT NULL  
+    ,teamid varchar(10) NULL
+); 

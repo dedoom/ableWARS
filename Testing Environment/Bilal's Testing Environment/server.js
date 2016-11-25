b79d3e7207055109fcf157684ab266d096b10ac4
Bil Al-Shaikh <bashaikh11@gmail.com>
2016-11-25 17:34:58 -0500

    update states

--- Testing Environment/Bilal's Testing Environment/server.js
+++ Testing Environment/Bilal's Testing Environment/server.js
@@ -18,7 +18,7 @@ var express = require('express'),
     server = require('http').Server(app),
     io = require('socket.io').listen(server),
     path = require('path'),
-	bodyParser = require("body-parser"),
+	bodyParser = require('body-parser'),
     sql = require('mssql');
 	
 //database specific info
@@ -85,11 +85,11 @@ io.on('connection', function (socket) {
 			team1: {
 				player1: { playerID: player1ID, socket: player1Socket, targetTimestamp: 0 }, 
 				player2: { playerID: player3ID, socket: player3Socket, targetTimestamp: 0 },
-				score: -1000},
+				score: -11},
 			team2: {
 				player1: {playerID: player2ID, socket: player2Socket, targetTimestamp: 0 },
 				player2: {playerID: player4ID, socket: socket, targetTimestamp: 0 },
-				score: -1000},
+				score: -11},
 			gameOver: false};
 		matches.push(match);
         numOfMatches++;
@@ -102,7 +102,7 @@ io.on('connection', function (socket) {
 		player3Socket.emit('newTargetShapeLives', targetLives);
         socket.emit('newTargetShapeLives', targetLives);
 		
-		var startTime = Date.now() + 4000;
+		var startTime = Date.now() + 2000;
         player1Socket.emit('startGame', { matchID: match['matchID'], shapes: shapes, playerID: player1ID, startTime: startTime });
 		player2Socket.emit('startGame', { matchID: match['matchID'], shapes: shapes, playerID: player2ID, startTime: startTime });
 		player3Socket.emit('startGame', { matchID: match['matchID'], shapes: shapes, playerID: player3ID, startTime: startTime });
@@ -281,9 +281,8 @@ io.on('connection', function (socket) {
 				opponentTeam = 1;
 			}
 		matches[matchIndex]['team' + team]['score'] = score;
-		if (matches[matchIndex]['team1']['score'] > -999 && matches[matchIndex]['team2']['score'] > -999) {
+		if (matches[matchIndex]['team1']['score'] > -11 && matches[matchIndex]['team2']['score'] > -11) {
 			if (matches[matchIndex]['team1']['score'] > matches[matchIndex]['team2']['score']){
-				console.log("team1: " + matches[matchIndex]['team1']['score'] + ", team2: " + matches[matchIndex]['team2']['score']);
 				match['team2']['player1']['socket'].emit('lose');
 				match['team2']['player2']['socket'].emit('lose');
 				match['team1']['player1']['socket'].emit('win');
@@ -293,10 +292,10 @@ io.on('connection', function (socket) {
 				ensureSplice(256);
 			}
 			else if (matches[matchIndex]['team1']['score'] < matches[matchIndex]['team2']['score']){
-				match['team2']['player1']['socket'].emit('win');
-				match['team2']['player2']['socket'].emit('win');
-				match['team1']['player1']['socket'].emit('lose');
-				match['team1']['player2']['socket'].emit('lose');
+				match['team1']['player1']['socket'].emit('win');
+				match['team1']['player2']['socket'].emit('win');
+				match['team2']['player1']['socket'].emit('lose');
+				match['team2']['player2']['socket'].emit('lose');
 				matches.splice(getIndexOfMatchByID(data['matchID']), 1);
 				numOfMatches--;
 				ensureSplice(265);
@@ -484,8 +483,8 @@ app.post('/signIn', function(req, res, next) {
 // *************************************************************************************** //
 // ******************************** REGISTRATION PORTION ******************************** //
 app.post('/signUp', function(req, res, next) {
-    var fname = req.body.firstName;
-    var lname = req.body.lastName;
+    var fname = null; //req.body.firstName;
+    var lname = null; //req.body.lastName;
     var uname = req.body.username;
     var pass = req.body.password;
     var teamid = null;
@@ -496,6 +495,7 @@ app.post('/signUp', function(req, res, next) {
 
     dbConn.connect().then(function() {
         var transaction = new sql.Transaction(dbConn);
+        
         transaction.begin().then(function() {
             var request = new sql.Request(dbConn);
             console.log("Transaction");
@@ -507,32 +507,27 @@ app.post('/signUp', function(req, res, next) {
                 }).catch(function (err) {
                     console.log("Error in Transaction Commit " + err);
                     dbConn.close();
-            });
-        }).catch(function (err) {
+                });
+            }).catch(function (err) {
                 console.log("Error in Transaction Begin " + err);
                 dbConn.close();
+                });
+            
+            }).catch(function (err) {
+                console.log(err);
+                dbConn.close();
             });
-        }).catch(function (err) {
-            console.log(err);
-            dbConn.close();
-        });
-
-        response = "200";
-        console.log(response);
-        res.end(response);
 
     }).catch(function (err) {
         console.log(err);
     });
+
+    response = "200";
+    console.log(response);
+    res.end(response);
 });
 
 app.post('/usernameValidation', function(req, res, next) {
-	if (req == null){
-		console.log("req null");
-	}
-	if (req.body == null){
-		console.log("req null");
-	}
     var uname = req.body.username;
     var response = "";
     
@@ -565,10 +560,71 @@ app.post('/usernameValidation', function(req, res, next) {
 // *************************************************************************************** //
 // ******************************** STATISTICS PORTION ******************************** //
 // username, 
-app.post('getStats', function(req, res, next) {
-    var username = req.body.username;
-    var response = {
-        uname : username,
+app.post('updateStats', function(req, res, next) {
+    var uname = req.body.username;
+    var state = req.body.state;
+    
+    var dbConn = new sql.Connection(config);
+    
+    dbConn.connect().then(function(){
+        var transactionStats = new sql.Transaction(dbConn);
         
-    }
+        transactionStats.begin().then(function() {
+            var request = new sql.Request(dbConn);
+            console.log("Transaction update Stats");
+            var forNull = null;
+            if (state == "win") {
+                request.query("UPDATE Statistics SET wins=wins+1, gamesplayed=gamesplayed+1 WHERE username='" + uname + "'");
+                .then(function() {
+                    transactionStates.commit().then(function(recordset) {
+                        console.log("Affected Rows: " + request.rowsAffected);
+                        dbConn.close();
+                    }).catch(function (err) {
+                        console.log("Error in Transaction Commit " + err);
+                        dbConn.close();
+                    });
+                    }).catch(function (err) {
+                        console.log("Error in Transaction Begin " + err);
+                        dbConn.close();
+                    });
+            } else if (state == "tie") {
+                request.query("UPDATE Statistics SET gamesplayed=gamesplayed+1 WHERE username='" + uname + "'");
+                .then(function() {
+                    transactionStates.commit().then(function(recordset) {
+                        console.log("Affected Rows: " + request.rowsAffected);
+                        dbConn.close();
+                    }).catch(function (err) {
+                        console.log("Error in Transaction Commit " + err);
+                        dbConn.close();
+                    });
+                    }).catch(function (err) {
+                        console.log("Error in Transaction Begin " + err);
+                        dbConn.close();
+                    });
+            } else if (state == "loss") {
+                request.query("UPDATE Statistics SET losses=losses+1, gamesplayed=gamesplayed+1 WHERE username='" + uname + "'");
+                .then(function() {
+                    transactionStates.commit().then(function(recordset) {
+                        console.log("Affected Rows: " + request.rowsAffected);
+                        dbConn.close();
+                    }).catch(function (err) {
+                        console.log("Error in Transaction Commit " + err);
+                        dbConn.close();
+                    });
+                    }).catch(function (err) {
+                        console.log("Error in Transaction Begin " + err);
+                        dbConn.close();
+                    });
+            }
+            }).catch(function (err) {
+                console.log(err);
+                dbConn.close();
+            });
+    }).catch(function (err) {
+        console.log(err);
+    });
+    
+    response = "200";
+    console.log(response);
+    res.end(response);
 });
